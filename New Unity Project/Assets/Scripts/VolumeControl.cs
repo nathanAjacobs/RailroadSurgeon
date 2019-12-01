c2ff04330eb0da0aa3e803f7c7046ee1155e27b6
Stormy <brenton.bushnell@gmail.com>
2019-12-01 15:07:06 -0500

    Volume Control Enabled
    Volume sliders fuckin work.

---
+++ New Unity Project/Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeControl : MonoBehaviour
+{
+    FMOD.Studio.Bus Music;
+    FMOD.Studio.Bus SFX;
+    FMOD.Studio.Bus Master;
+    float MusicVolume = 0.5f;
+    float SFXVolume = 0.5f;
+    float newMusicVolume;
+    float newSFXVolume;
+
+    void Awake()
+    {
+        Music = FMODUnity.RuntimeManager.GetBus("bus:/Music");
+        SFX = FMODUnity.RuntimeManager.GetBus("bus:/SFX");
+    }
+
+
+    void Update()
+    {
+        Music.setVolume(MusicVolume);
+        SFX.setVolume(SFXVolume);
+
+    }
+
+
+    public void MusicVolumeControl(float newMusicVolume)
+    {
+        MusicVolume = newMusicVolume;
+    }
+
+    public void SFXVolumeControl(float newSFXVolume)
+    {
+        SFXVolume = newSFXVolume;
+    }
+
+}
